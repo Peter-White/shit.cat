52bf934d52b4fd03c7fb982e35ce78e5fada1cc2
Peter Masucci <peter.a.masucci@gmail.com>
2015-06-17 22:17:42 -0400

    Nearly Complete. Needs moar cats

---
+++ js/shitcat.js
@@ -0,0 +1,14 @@
+var m = document.getElementById("shitcat");
+var c = m.style;
+var shitcats = ["http://i.imgur.com/Gy98CEc.jpg", "http://i.imgur.com/t2XZqTj.jpg"];
+c.backgroundImage = "url(" + shitcats[cat(0, shitcats.length)] + ")";
+c.backgroundPosition = "center center";
+c.backgroundRepeat = "no-repeat";
+c.backgroundSize = "contain";
+
+
+console.log(cat(0,shitcats.length));
+
+function cat(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
