5a757f9c33f86ae08358fdb01aee43335f3a8ae7
Hogue <ajhogue6@gmail.com>
2015-06-29 21:56:45 -0400

    adds two new shit.cats

--- js/shitcat.js
+++ js/shitcat.js
@@ -1,6 +1,6 @@
 var m = document.getElementById("shitcat");
 var c = m.style;
-var shitcats = ["http://i.imgur.com/Gy98CEc.jpg", "http://i.imgur.com/t2XZqTj.jpg", "http://i.imgur.com/knntFzQ.jpg", "http://i.imgur.com/jElaj6c.jpg"];
+var shitcats = ["http://i.imgur.com/Gy98CEc.jpg", "http://i.imgur.com/t2XZqTj.jpg", "http://i.imgur.com/knntFzQ.jpg", "http://i.imgur.com/jElaj6c.jpg", "http://i.imgur.com/M2EUnTe.jpg", "http://i.imgur.com/Osw6ujp.jpg"];
 c.backgroundImage = "url(" + shitcats[cat(0, shitcats.length)] + ")";
 c.backgroundPosition = "center center";
 c.backgroundRepeat = "no-repeat";
