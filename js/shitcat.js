c762205cb9506df9755be20b951d28a62b224d76
tuskerette <valeria.graffeo@gmail.com>
2015-08-29 13:03:02 +0200

    added Pardo shit cat

--- js/shitcat.js
+++ js/shitcat.js
@@ -1,6 +1,6 @@
 var m = document.getElementById("shitcat");
 var c = m.style;
-var shitcats = ["http://i.imgur.com/Gy98CEc.jpg", "http://i.imgur.com/t2XZqTj.jpg", "http://i.imgur.com/reBm0Tb.gif", "http://i.imgur.com/knntFzQ.jpg", "http://i.imgur.com/jElaj6c.jpg", "http://i.imgur.com/M2EUnTe.jpg", "http://i.imgur.com/Osw6ujp.jpg", "http://i.imgur.com/f2geyi1.jpg", "http://i.imgur.com/mEX9IUz.jpg"];
+var shitcats = ["http://i.imgur.com/Gy98CEc.jpg", "http://i.imgur.com/t2XZqTj.jpg", "http://i.imgur.com/reBm0Tb.gif", "http://i.imgur.com/knntFzQ.jpg", "http://i.imgur.com/jElaj6c.jpg", "http://i.imgur.com/M2EUnTe.jpg", "http://i.imgur.com/Osw6ujp.jpg", "http://i.imgur.com/f2geyi1.jpg", "http://i.imgur.com/mEX9IUz.jpg", "http://i.imgur.com/3xtGQ2W.jpg"];
 c.backgroundImage = "url(" + shitcats[cat(0, shitcats.length)] + ")";
 c.backgroundPosition = "center center";
 c.backgroundRepeat = "no-repeat";
